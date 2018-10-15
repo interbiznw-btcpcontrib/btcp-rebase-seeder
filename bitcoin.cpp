c4db5680f1f8cf494d52473e0b5d8deacaf86045
J62 <j.owens@interbiznw.com>
2018-10-15 12:30:48 -0700

    Update bitcoin.cpp

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -82,7 +82,7 @@ class CNode {
     CAddress me(CService("0.0.0.0"));
     BeginMessage("version");
     int nBestHeight = GetRequireHeight();
-    string ver = "/bitcoin-seeder:0.01/";
+    string ver = "/btcp-rebase-seeder:0.01/";
     vSend << PROTOCOL_VERSION << nLocalServices << nTime << you << me << nLocalNonce << ver << nBestHeight;
     EndMessage();
   }
