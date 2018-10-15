006658ec284fd9bbe22ff7f8d5fcd81060f2b249
J62 <j.owens@interbiznw.com>
2018-10-15 12:36:17 -0700

    Update protocol.cpp

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xc7, 0xe4, 0xba, 0xf8 };
+unsigned char pchMessageStart[4] = { 0xa8, 0xea, 0xa2, 0xcd };
 
 CMessageHeader::CMessageHeader()
 {
